bbdf51a1ed22bd7fa884cf4a1c0e2f3d1a2536c6
EYPEKR <eyuepeker@hotmail.de>
2021-07-27 10:31:55 +0200

    added druid to the project

--- fifteen_puzzle/src/main.rs
+++ fifteen_puzzle/src/main.rs
@@ -1,6 +1,24 @@
 mod model;
 mod controller;
+use druid::{AppLauncher, WindowDesc, Widget, PlatformError};
+use druid::widget::{Label,Flex,Padding};
 
-fn main() {
+
+fn build_ui() -> impl Widget<()> {
+    Padding::new(100.0,
+        Flex::row().with_flex_child(
+            Flex::column()
+                .with_flex_child(Label::new("top left"), 1.0)
+                .with_flex_child(Label::new("bottom left"), 1.0),
+            1.0)
+            .with_flex_child(Flex::column()
+            .with_flex_child(Label::new("top right"), 1.0)
+            .with_flex_child(Label::new("bottom right"), 1.0),
+            1.0))
+}
+fn main() -> Result<(), PlatformError>{
     let field = model::create_field();
+    AppLauncher::with_window(WindowDesc::new(||{build_ui()})).launch(())?;
+
+    Ok(())
 }
