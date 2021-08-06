a74225183800b771bc77468b7c641bb5f650dc3e
Eyüp Eker <eyuep.eker@stud.th-luebeck.de>
2021-08-06 05:04:25 +0200

    changed size of window, reformatted code

--- fifteen_puzzle/src/model.rs
+++ fifteen_puzzle/src/model.rs
@@ -1,17 +1,17 @@
-use self::field::Tilegrid;
+pub use self::field::Tilegrid as Tg;
 
 
 pub mod field;
 pub mod point;
 
 fn init() -> field::Tilegrid{
-    Tilegrid::new()
+    Tg::new()
 }
 
 use rand::Rng;
-fn mix(mut tg: field::Tilegrid) -> field::Tilegrid{
+fn mix(mut tg: Tg) -> field::Tilegrid{
     let mut rng =rand::thread_rng();
-    let iterations = rng.gen_range(50..200);
+    let iterations = rng.gen_range(0..2);
     for _ in 0..iterations{
         let e = tg.get_empty();
         let dir = rng.gen_range(0..4); 
--- fifteen_puzzle/src/view.rs
+++ fifteen_puzzle/src/view.rs
@@ -1,20 +1,11 @@
+use super::model;
+
 use druid::{AppLauncher, PlatformError, Widget, WidgetExt, WindowDesc};
-use druid::widget::{Label, Flex, Padding};
+use druid::widget::{Label, Flex};
+
 
 use super::model::{field::Tilegrid as Tg, point::Point};
 
-fn _build_ui() -> impl Widget<()> {
-    Padding::new(100.0,
-        Flex::row().with_flex_child(
-            Flex::column()
-                .with_flex_child(Label::new("top left"), 1.0)
-                .with_flex_child(Label::new("bottom left"), 1.0),
-            1.0)
-            .with_flex_child(Flex::column()
-            .with_flex_child(Label::new("top right"), 1.0)
-            .with_flex_child(Label::new("bottom right"), 1.0),
-            1.0))
-}
 
 fn get_str(val:u32)->String{
     match val {
@@ -29,6 +20,9 @@ fn get_label(x:usize, y:usize)-> impl Widget<Tg>{
     return Label::new(move |data:&Tg, _env:&_| get_str(data.get_field()[x.clone()][y.clone()]))
         .fix_height(100.0).fix_width(100.0)
         .align_horizontal(druid::UnitPoint::CENTER)
+        .align_vertical(druid::UnitPoint::CENTER)
+        .center()
+        //.layout(ctx, bc, data, env)
         .border(druid::Color::grey8(255),2.0 )
         .on_click(move|_ctx, data:&mut Tg,_env| data.move_tile(Point::new(x as u8,y as u8)))
 }
@@ -59,6 +53,14 @@ pub fn build_ui() -> impl Widget<Tg>{
             .with_flex_child(get_label(3,2), 1.0)
             .with_flex_child(get_label(3,3), 1.0)
             , 1.0)
-    
-    
+}
+
+pub fn launch_app(field:model::Tg) -> Result<(), PlatformError>{
+    AppLauncher::with_window(
+        WindowDesc::new(||{build_ui()})
+            .title("Fifteen Puzzle")
+            .window_size((400.0, 400.0))
+            .resizable(false)
+    ).use_simple_logger().launch(field)?;
+    Ok(())
 }
